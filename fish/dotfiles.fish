b30f44289358ccfb4983ce69b3b9236dd5acee23
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-07-01 18:14:16 -0300

    fix: export envs
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- fish/dotfiles.fish
+++ fish/dotfiles.fish
@@ -1,15 +1,15 @@
 set -g pure_symbol_prompt λ
 set -g pure_threshold_command_duration 1
 
-set EDITOR vim
-set VEDITOR code
+set -x EDITOR vim
+set -x VEDITOR code
 
-set DOTFILES ~/.dotfiles
-set PROJECTS ~/Code
-set GOPATH $PROJECTS/Go
+set -x DOTFILES ~/.dotfiles
+set -x PROJECTS ~/Code
+set -x GOPATH $PROJECTS/Go
 set PATH $PATH $GOPATH/bin
 
-set MAVEN_OPTS -Xmx1024m
+set -x MAVEN_OPTS -Xmx1024m
 
 set PATH $HOME/.krew/bin $PATH
 
