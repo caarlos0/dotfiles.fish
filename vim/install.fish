0f22f0de6c2028ba124ca8004e460a66b4926cdd
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-07-28 19:00:45 -0300

    fix: improve vim install
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- vim/install.fish
+++ vim/install.fish
@@ -1,8 +1,7 @@
 #!/usr/bin/env fish
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +'PlugInstall --sync' +qa
-vim +'PlugUpdate' +qa
+vim +'PlugUpdate | PlugInstall --sync' +qa -s
 
 if command -q nvim
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -10,6 +9,5 @@ if command -q nvim
 
 	mkdir -p ~/.config/nvim/
 	ln -sf "$DOTFILES/vim/vimrc.symlink" ~/.config/nvim/init.vim
-	nvim +'PlugInstall --sync' +qa
-	nvim +'PlugUpdate' +qa
+	nvim +'PlugUpdate | PlugInstall --sync' +qa
 end
