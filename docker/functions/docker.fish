1fba1f611d07ca837caf26e508460e94cb8b53c2
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-07-30 23:57:27 -0300

    feat: auto-start docker
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- docker/functions/docker.fish
+++ docker/functions/docker.fish
@@ -1,4 +1,12 @@
 function docker -w docker
+	if test -z (pgrep com.docker.hyperkit)
+		open -g -a Docker.app
+		while ! command docker stats --no-stream >/dev/null 2>&1
+			echo -n .
+			sleep 1
+		end
+		echo
+	end
 	switch $argv[1]
 	case prune
 		command docker system prune --volumes -fa
