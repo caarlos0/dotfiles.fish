4f0b8f2eee3e357f0d74fd033aff464facea8c27
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-08-06 14:29:26 -0300

    fix: ke abbr
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- kubernetes/conf.fish
+++ kubernetes/conf.fish
@@ -7,7 +7,7 @@ end
 
 abbr --add k kubectl
 abbr --add sk 'kubectl -n kube-system'
-abbr --add ke 'EDITOR=vim kubectl edit'
+abbr --add ke 'kubectl edit'
 abbr --add kdebug 'kubectl run -i -t debug --rm --image=caarlos0/debug --restart=Never'
 abbr --add knrunning 'kubectl get pods --field-selector=status.phase!=Running'
 abbr --add kfails 'kubectl get po -owide --all-namespaces | grep "0/" | tee /dev/tty | wc -l'
