7e39508c7fe85dc0cd3e87f91133491a6a63617f
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-08-10 09:53:57 -0300

    fix: remove nvim configs
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- vim/install.fish
+++ vim/install.fish
@@ -2,12 +2,3 @@
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugUpdate | PlugInstall --sync' +qa
-
-if command -q nvim
-	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-	mkdir -p ~/.config/nvim/
-	ln -sf "$DOTFILES/vim/vimrc.symlink" ~/.config/nvim/init.vim
-	nvim +'PlugUpdate | PlugInstall --sync' +qa
-end
