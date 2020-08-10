74fac9057c295c9077cb873b0e61f8b362c9c33c
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-08-10 19:05:19 -0300

    fix: always override fonts
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- fonts/install.fish
+++ fonts/install.fish
@@ -1,15 +1,15 @@
 #!/usr/bin/env fish
 function install
 	curl -Lso /tmp/mono.zip https://github.com/JetBrains/JetBrainsMono/releases/download/v1.0.6/JetBrainsMono-1.0.6.zip
-		and unzip -j /tmp/mono.zip '*/ttf/*' -d $argv[1]
+		and unzip -o -j /tmp/mono.zip '*/ttf/*' -d $argv[1]
 
 	curl -Lso /tmp/hack.zip https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
-		and unzip -j /tmp/hack.zip -d $argv[1]
+		and unzip -o -j /tmp/hack.zip -d $argv[1]
 end
 
 switch (uname)
 case Darwin
-	if command -q brew
+	if command -qs brew
 		brew tap -q homebrew/cask-fonts
 			and brew cask install font-jetbrains-mono font-hack
 	else
