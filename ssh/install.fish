5a9574496f9bbcd581fd724d4f53cf1c39ab8ec0
Dalton Sena <daltonsena@users.noreply.github.com>
2020-09-02 22:03:37 -0300

    refactor: test if ~/.ssh folder exist
    If applied, this commit will...
    
    Add a if exist test for the ~/.ssh folder. If folder doesn't exist creates a new one
    
    Why is this change being made?
    
    When the user has not made any ssh configuration (probably a fresh install of OS) the install process stops on .ssh configuration step

--- ssh/install.fish
+++ ssh/install.fish
@@ -1,6 +1,16 @@
 #!/usr/bin/env fish
-test -L ~/.ssh/config || {
-	mv ~/.ssh/config ~/.ssh/config.local
-	ln -s $DOTFILES/ssh/config ~/.ssh/config
-}
-test -f ~/.ssh/config.local || touch ~/.ssh/config.local
+
+function ssh_config -d "links ~/.ssh config file and keep a backup"
+	echo $argv | read -l p
+	if not test -e ~/.ssh
+		mkdir ~/.ssh
+		success "A new ~/.ssh folder has been created"
+	end
+	test -L ~/.ssh/config || {
+		mv ~/.ssh/config ~/.ssh/config.local
+		ln -s $DOTFILES/ssh/config ~/.ssh/config
+	}
+	test -f ~/.ssh/config.local || touch ~/.ssh/config.local
+end
+
+ssh_config  ~/.ssh/config.local
