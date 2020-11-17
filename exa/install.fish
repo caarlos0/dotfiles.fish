0a7cdf2dd5faa49936da9b80c40d10965b3c2eef
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-11-17 14:41:42 -0300

    fix: exa abbrs
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- exa/install.fish
+++ exa/install.fish
@@ -1,10 +1,11 @@
 #!/usr/bin/env fish
 if command -qa exa
-	abbr -a ls 'exa --git'
-	abbr -a l 'exa -lah --git'
-	abbr -a la 'exa -a --git'
-	abbr -a ll 'exa -l --git'
-	abbr -a lt 'exa -lT --git'
+	abbr -a ls 'exa'
+	abbr -a lg 'exa --git'
+	abbr -a l 'exa -lah'
+	abbr -a la 'exa -a'
+	abbr -a ll 'exa -l'
+	abbr -a lt 'exa -lT'
 else
 	abbr -a l 'ls -lAh'
 	abbr -a la 'ls -A'
