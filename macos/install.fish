7e880b07b9c5cab3f2653c3f9393c718a066b3a6
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-12-05 12:45:21 -0300

    feat: add /usr/local/bin to fish_user_paths
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- macos/install.fish
+++ macos/install.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 abbr -a airport /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
 abbr -a afk 'open -a /System/Library/CoreServices/ScreenSaverEngine.app'
+
+sudo mkdir -p /usr/local/sbin
+sudo chown -R (whoami) /usr/local/sbin
+set -U fish_user_paths /usr/local/sbin $fish_user_paths
