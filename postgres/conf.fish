abf0b73af588808e7ada209eac920b3bb94297d9
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-12-05 13:59:06 -0300

    fix: postgres: move conf to install
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- postgres/conf.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-if test -e "/Applications/Postgres.app"
-    set -a PATH /Applications/Postgres.app/Contents/Versions/latest/bin/
-end
--- postgres/install.fish
+++ postgres/install.fish
@@ -2,3 +2,6 @@
 if ! test -e ~/.psqlrc.local
 	touch ~/.psqlrc.local
 end
+if test -e "/Applications/Postgres.app"
+    set -Ua PATH /Applications/Postgres.app/Contents/Versions/latest/bin/
+end
