93dd1013df58e5c4744e7af5165a163434aaf6bb
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-01-20 15:03:21 -0300

    fix: user paths
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- macos/install.fish
+++ macos/install.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 abbr -a airport /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
 abbr -a afk 'open -a /System/Library/CoreServices/ScreenSaverEngine.app'
-set -U fish_user_paths /usr/local/sbin $fish_user_paths
+set -Ua fish_user_paths /usr/local/sbin
