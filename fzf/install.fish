18d0151cdadcc573e39bfffd133c96cdd538d0c5
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-02-18 19:52:23 -0300

    feat: back to dracula
    its more readable than nord.
    
    BREAKING CHANGE: replace nord with dracula
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- fzf/install.fish
+++ fzf/install.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-set -Ux FZF_DEFAULT_OPTS "--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b"
+set -Ux FZF_DEFAULT_OPTS "--color fg:255,bg:236,hl:84,fg+:255,bg+:236,hl+:215 --color info:141,prompt:84,spinner:212,pointer:212,marker:212"
