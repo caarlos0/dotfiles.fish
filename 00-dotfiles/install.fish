5e12a218ed40ec9032b6b3ae15bfccac99e4b3bb
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-04-05 16:31:51 -0300

    feat: starship
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- 00-dotfiles/install.fish
+++ 00-dotfiles/install.fish
@@ -12,6 +12,10 @@ for f in $DOTFILES/*/functions
 	set -Up fish_function_path $f
 end
 
+for f in $DOTFILES/*/conf.d/*.fish
+	ln -sf $f ~/.config/fish/conf.d/(basename $f)
+end
+
 if test -f ~/.localrc.fish
 	ln -sf ~/.localrc.fish ~/.config/fish/conf.d/localrc.fish
 end
---
+++ starship/install.fish
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+set -Ux STARSHIP_CONFIG $DOTFILES/starship/config.toml
