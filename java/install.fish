525fc33418aeb5316a866f978424c96d719ec19b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-04-12 08:53:11 -0300

    feat: starship prompt (#7)
    * feat: update vscode settings
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * feat: starship
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: config
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: starship config
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * chore: vscode settings
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: starship
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: java home
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: vscode
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: git settings
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: conf
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * feat: update iterm config
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: improve readme
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- java/install.fish
+++ java/install.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 abbr -a mvnci 'mvn clean install'
 set -Ux MAVEN_OPTS -Xmx1024m
+
+if command -qs /usr/libexec/java_home
+	set -Ux JAVA_HOME (/usr/libexec/java_home -v 11)
+end
---
+++ starship/conf.d/starship.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+if command -q starship
+    starship init fish | source
+end
