ffbbf8aeff35cb5b078f2b5c87ad6ccf0c1cfe66
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-05-09 21:33:30 -0300

    fix: fzf format
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- fzf/install.fish
+++ fzf/install.fish
@@ -1,6 +1,3 @@
 #!/usr/bin/env fish
 
-set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9
-  --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
-  --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
-  --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
