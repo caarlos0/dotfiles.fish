a24b008490f66e7a77531d804e8d02decc96dd75
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-06-26 19:00:13 -0300

    fix: fish 3.2 updates
    refs https://github.com/caarlos0/dotfiles.fish/issues/9
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

---
+++ fish/install.fish
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+set -U fish_greeting
--- gh/install.fish
+++ gh/install.fish
@@ -1,6 +1,4 @@
 #!/usr/bin/env fish
 if command -qs gh
-	gh completion -s fish > ~/.config/fish/completions/gh.fish
-
 	gh config set git_protocol ssh
 end
