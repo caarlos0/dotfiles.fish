9b63e68737efb5d3a5ed0cc69dac2cdf1e69532a
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-08-09 09:10:50 -0300

    feat: dc alias
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- docker/install.fish
+++ docker/install.fish
@@ -2,3 +2,4 @@
 curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish -o ~/.config/fish/completions/docker.fish
 
 abbr -a d docker
+abbr -a dc docker-compose
