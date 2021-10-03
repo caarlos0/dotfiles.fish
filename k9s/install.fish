1f37f414c072ea3766df2a6dfe583011bdb2171b
Carlos A Becker <caarlos0@gmail.com>
2021-10-03 11:03:24 -0300

    fix: java and k9s install scriptts
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- k9s/install.fish
+++ k9s/install.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 switch (uname)
 case Darwin
-	if test -d $XDG_CONFIG_HOME/k9s
+	if test -d ~/Library/Preferences/k9s
 		curl -sL https://raw.githubusercontent.com/derailed/k9s/master/skins/dracula.yml -o ~/Library/Preferences/k9s/skin.yml
 	end
 case Linux
