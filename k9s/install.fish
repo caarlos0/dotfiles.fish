a4f41e31e8be37bbbacfe16fda1271f9599c0f90
Carlos A Becker <caarlos0@gmail.com>
2021-10-20 08:52:12 -0300

    fix: k9s install

--- k9s/install.fish
+++ k9s/install.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
-switch (uname)
-case Darwin
-	if test -d ~/Library/Preferences/k9s
+if command -qs k9s
+	switch (uname)
+	case Darwin
+		mkdir -p ~/Library/Preferences/k9s
 		curl -sL https://raw.githubusercontent.com/derailed/k9s/master/skins/dracula.yml -o ~/Library/Preferences/k9s/skin.yml
-	end
-case Linux
-	if test -d $XDG_CONFIG_HOME/k9s
+	case Linux
+		mkdir -p $XDG_CONFIG_HOME/k9s
 		curl -sL https://raw.githubusercontent.com/derailed/k9s/master/skins/dracula.yml -o $XDG_CONFIG_HOME/k9s/skin.yml
 	end
 end
