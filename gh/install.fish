d227ab91969c1fd71f43d99c1d98d3ec8cce6027
Carlos A Becker <caarlos0@gmail.com>
2021-11-23 22:27:11 -0300

    feat: gh prs plugin
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- gh/install.fish
+++ gh/install.fish
@@ -2,7 +2,16 @@
 if command -qs gh
 	gh config set git_protocol ssh
 
-	for ext in mislav/gh-branch vilmibm/gh-user-status
+	for ext in mislav/gh-branch vilmibm/gh-user-status dlvhdr/gh-prs
 		gh extension install $ext || true
 	end
+end
+
+switch (uname)
+case Darwin
+	mkdir -p "$HOME/Library/Application Support/prs"
+	ln -sf "$DOTFILES/gh/prs.yml" "$HOME/Library/Application Support/prs/sections.yml"
+case Linux
+	mkdir -p "$HOME/.config/prs"
+	ln -sf "$DOTFILES/gh/prs.yml" "$HOME/.config/prs/sections.yml"
 end
