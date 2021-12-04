62acc07e7dc6019bdd848bd92cdd59e328ff9dcd
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-12-03 23:27:23 -0300

    feat: clean up things (#11)
    * fix: cleaning up stuff a bit
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>
    
    * fix: vim
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- java/install.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-abbr -a mvnci 'mvn clean install'
-set -Ux MAVEN_OPTS -Xmx1024m
-
-if command -qs /usr/libexec/java_home
-    if /usr/libexec/java_home >/dev/null 2>&1
-    	set -Ux JAVA_HOME (/usr/libexec/java_home -v 11)
-    end
-end
--- system/install.fish
+++ system/install.fish
@@ -1,2 +1,5 @@
 #!/usr/bin/env fish
 abbr -a less 'less -r'
+abbr -a l 'ls -lAh'
+abbr -a la 'ls -A'
+abbr -a ll 'ls -l'
