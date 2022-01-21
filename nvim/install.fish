6b1633b246be9b85df4dcdfec83d6111e4f894ce
Carlos A Becker <caarlos0@gmail.com>
2022-01-21 16:10:13 -0300

    fix: nvim->vim
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- nvim/install.fish
+++ nvim/install.fish
@@ -2,3 +2,4 @@
 curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +'PlugUpdate | PlugInstall --sync' +qa
+abbr -a kx kubectx
