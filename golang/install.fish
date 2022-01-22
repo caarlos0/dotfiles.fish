fa5a3b091e234491e3e6d72788f4eb22b282b5e3
Carlos A Becker <caarlos0@gmail.com>
2022-01-21 23:13:56 -0300

    wip
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- golang/install.fish
+++ golang/install.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 set -Ux GOPATH $PROJECTS/Go
 set -Ua fish_user_paths $GOPATH/bin
+
+go install github.com/psampaz/go-mod-outdated@v0.8.0
+
+go list -u -m -json all | go-mod-outdated -style markdown -direct -update | glow --width 120 -
