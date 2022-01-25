0e4f41c0e025390296b9be7ecadd8d0e704dc357
Carlos A Becker <caarlos0@gmail.com>
2022-01-24 21:58:36 -0300

    Revert "fix: lspconfig handle the organize imports thing already"
    This reverts commit 856ed6c451fb2b84f0a4ad5ebf915c39900d5c36.

--- nvim/config/lua/rc/lsp.lua
+++ nvim/config/lua/rc/lsp.lua
@@ -43,6 +43,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    -- vim.api.nvim_command [[autocmd BufWritePre <buffer> lua organizeImports(1000)]]
     vim.api.nvim_command [[augroup END]]
   end
 
@@ -93,3 +94,34 @@ nvim_lsp['bashls'].setup {
   on_attach = on_attach,
 }
 
+
+-- organize imports
+function organizeImports(timeoutms)
+	local context = { source = { organizeImports = true } }
+	vim.validate { context = { context, "t", true } }
+
+	local params = vim.lsp.util.make_range_params()
+	params.context = context
+
+	-- See the implementation of the textDocument/codeAction callback
+	-- (lua/vim/lsp/handler.lua) for how to do this properly.
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+	if not result or next(result) == nil then return end
+	local actions = result[1].result
+	if not actions then return end
+	local action = actions[1]
+
+	-- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+	-- is a CodeAction, it can have either an edit, a command or both. Edits
+	-- should be executed first.
+	if action.edit or type(action.command) == "table" then
+	  if action.edit then
+		vim.lsp.util.apply_workspace_edit(action.edit)
+	  end
+	  if type(action.command) == "table" then
+		vim.lsp.buf.execute_command(action.command)
+	  end
+	else
+	  vim.lsp.buf.execute_command(action)
+	end
+end
