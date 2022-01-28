4f4f665e74ae188e7bdb8cca3c57380682c708e2
Carlos A Becker <caarlos0@gmail.com>
2022-01-27 21:57:47 -0300

    fix: errors when lsp restarts

--- nvim/config/lua/rc/lsp.lua
+++ nvim/config/lua/rc/lsp.lua
@@ -40,35 +40,37 @@ local on_attach = function(client, bufnr)
   -- buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
   if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua organizeImports(1000)]]
-    vim.api.nvim_command [[augroup END]]
+    vim.api.nvim_exec([[
+    augroup formatting
+      autocmd! * <buffer>
+      autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
+      autocmd BufWritePre <buffer> lua organizeImports(1000)
+    augroup END
+    ]], false)
   end
 
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
-	vim.api.nvim_exec([[
-		hi LspReferenceText ctermfg=0 ctermbg=11 guifg=Black guibg=Yellow
-		hi LspReferenceRead ctermfg=0 ctermbg=11 guifg=Black guibg=Yellow
-		hi LspReferenceWrite ctermfg=0 ctermbg=11 guifg=Black guibg=Yellow
-		augroup lsp_document_highlight
-			autocmd! * <buffer>
-			autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-			autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-		augroup END
-	]], false)
+    vim.api.nvim_exec([[
+    hi LspReferenceText ctermfg=0 ctermbg=11 guifg=Black guibg=Yellow
+    hi LspReferenceRead ctermfg=0 ctermbg=11 guifg=Black guibg=Yellow
+    hi LspReferenceWrite ctermfg=0 ctermbg=11 guifg=Black guibg=Yellow
+    augroup lsp_document_highlight
+      autocmd! * <buffer>
+      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    augroup END
+    ]], false)
   end
 end
 
 
 nvim_lsp['gopls'].setup {
-	capabilities = capabilities,
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150,
-	}
+  capabilities = capabilities,
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  }
 }
 
 local schemas = {}
@@ -97,31 +99,19 @@ nvim_lsp['bashls'].setup {
 
 -- organize imports
 function organizeImports(timeoutms)
-	local context = { source = { organizeImports = true } }
-	vim.validate { context = { context, "t", true } }
-
-	local params = vim.lsp.util.make_range_params()
-	params.context = context
-
-	-- See the implementation of the textDocument/codeAction callback
-	-- (lua/vim/lsp/handler.lua) for how to do this properly.
-	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-	if not result or next(result) == nil then return end
-	local actions = result[1].result
-	if not actions then return end
-	local action = actions[1]
-
-	-- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-	-- is a CodeAction, it can have either an edit, a command or both. Edits
-	-- should be executed first.
-	if action.edit or type(action.command) == "table" then
-	  if action.edit then
-		vim.lsp.util.apply_workspace_edit(action.edit)
-	  end
-	  if type(action.command) == "table" then
-		vim.lsp.buf.execute_command(action.command)
-	  end
-	else
-	  vim.lsp.buf.execute_command(action)
-	end
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit)
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
 end
+
+
+-- vim:set ft=lua et sw=2 tabstop=2:
