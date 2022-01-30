f2210eca43556a93acb539cdfe3c7a99c7e19845
Carlos A Becker <caarlos0@gmail.com>
2022-01-30 00:42:56 -0300

    feat: autocommends for git commit and markdown

--- nvim/config/lua/user/autocommands.lua
+++ nvim/config/lua/user/autocommands.lua
@@ -5,9 +5,21 @@ fun! TrimWhitespace()
   call winrestview(l:save)
 endfun
 
-augroup DOTFILES
+augroup _general_settings
   autocmd!
   autocmd BufWritePre * :call TrimWhitespace()
   autocmd BufWritePre * call mkdir(expand("<afile>:p:h"), "p")
+augroup END
+
+augroup _git
+  autocmd!
+  autocmd FileType NeogitCommitMessage setlocal wrap
+  autocmd FileType NeogitCommitMessage setlocal spell
+augroup END
+
+augroup _markdown
+  autocmd!
+  autocmd FileType markdown setlocal wrap
+  autocmd FileType markdown setlocal spell
 augroup END
 ]])
