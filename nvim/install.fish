795d9fbf6f40f00b84612fe080d0c79d0977d10b
Carlos A Becker <caarlos0@gmail.com>
2022-01-30 16:35:49 -0300

    fix: nvim install
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- nvim/install.fish
+++ nvim/install.fish
@@ -1,8 +1,4 @@
 #!/usr/bin/env fish
-curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +'PlugUpdate | PlugInstall --sync' +qa
-
 ln -sf (which nvim) (which vim) or
 	sudo ln -sf (which nvim) (which vim)
 
