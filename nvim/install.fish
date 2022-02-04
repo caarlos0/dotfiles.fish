1a3cd92712f6f499ca6eda562fbcc3be27a048a5
Carlos A Becker <caarlos0@gmail.com>
2022-02-04 12:06:47 -0300

    fix: nvim/install
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- nvim/install.fish
+++ nvim/install.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-ln -sf (which nvim) (which vim) or
-	sudo ln -sf (which nvim) (which vim)
+ln -sf (which nvim) (which vim)
+	or sudo ln -sf (which nvim) (which vim)
 
 abbr -a v 'nvim'
