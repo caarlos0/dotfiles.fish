64b57cf1de643b2b3425a6c1a026e244baee3e14
Carlos A Becker <caarlos0@gmail.com>
2022-02-07 10:28:19 -0300

    fix: hi current line nr
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- nvim/config/lua/user/colorscheme.lua
+++ nvim/config/lua/user/colorscheme.lua
@@ -5,15 +5,21 @@ try
   packadd! dracula_pro
   let g:dracula_colorterm = 0
   colorscheme dracula_pro
-  " fixes lsp referecens on dracula-pro
-  hi! link LspReferenceText DraculaSelection
-  hi! link LspReferenceRead DraculaSelection
-  hi! link LspReferenceWrite DraculaSelection
 catch
   try
     colorscheme dracula
   catch
     colorscheme default
   endtry
+endtry
+
+try
+  " fixes lsp referecens on dracula-pro
+  hi! link LspReferenceText DraculaSelection
+  hi! link LspReferenceRead DraculaSelection
+  hi! link LspReferenceWrite DraculaSelection
+  " highlights current line number
+  highlight CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+  set cursorline
 endtry
 ]])
