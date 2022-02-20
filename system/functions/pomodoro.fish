1b77bdb034563182af62e66292f1009a73644a62
Carlos A Becker <caarlos0@gmail.com>
2022-02-19 21:40:50 -0300

    fix: pomodoro fn
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- system/functions/pomodoro.fish
+++ system/functions/pomodoro.fish
@@ -1,10 +1,10 @@
 function pomodoro -d "a cli pomodoro"
 	while true
 		say "Let's go!"
-		timer --for 50m --name Working
+		timer 50m --name Working
 		say "Break!"
 		terminal-notifier -message "Time to rest..." -title Pomodoro -subtitle "Done!" -ignoreDnd -sound default
-		timer --for 10m --name Break
+		timer 10m --name Break
 		terminal-notifier -message "Let's get back to it" -title Pomodoro -subtitle "Let's go!" -ignoreDnd -sound default
 	end
 end
