8e84414947923867c89f2b1c9d778821bf05d116
Carlos A Becker <caarlos0@gmail.com>
2022-02-27 13:42:34 -0300

    fix: go install
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- golang/install.fish
+++ golang/install.fish
@@ -1,11 +1,3 @@
 #!/usr/bin/env fish
 set -Ux GOPATH $PROJECTS/Go
-set -Ua fish_user_paths $GOPATH/bin
-
-if test -d /usr/local/go/bin
-	set -Ua fish_user_paths /usr/local/go/bin
-end
-
-if command -qs go
-	go install github.com/nametake/golangci-lint-langserver@latest
-end
+set -Ua fish_user_paths $GOPATH/bin /usr/local/go/bin
