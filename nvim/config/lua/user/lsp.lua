91d806fab6daf941ff9bdf252675aa3abeba814e
Carlos A Becker <caarlos0@gmail.com>
2022-03-12 22:41:13 -0300

    feat: more cmp

--- nvim/config/lua/user/lsp.lua
+++ nvim/config/lua/user/lsp.lua
@@ -61,7 +61,7 @@ local capabilities = vim.tbl_extend(
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.completion.spell,
+		-- null_ls.builtins.completion.spell,
 		-- this breaks organize imports, for some reason
 		-- null_ls.builtins.code_actions.gitsigns,
 	},
