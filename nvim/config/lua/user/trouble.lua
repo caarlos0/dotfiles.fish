fdd70606f4d3a0ed61554aee7ddd1c93e710ad77
Carlos A Becker <caarlos0@gmail.com>
2022-03-20 00:30:26 -0300

    feat: other trouble keys

--- nvim/config/lua/user/trouble.lua
+++ nvim/config/lua/user/trouble.lua
@@ -5,4 +5,9 @@ end
 
 trouble.setup({})
 
-require("user.remap").nnoremap("<leader>xx", "<cmd>TroubleToggle<CR>")
+local r = require("user.remap").nnoremap
+r("<leader>xx", "<cmd>TroubleToggle<CR>")
+r("<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>")
+r("<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>")
+r("<leader>xq", "<cmd>TroubleToggle quickfix<CR>")
+r("<leader>xl", "<cmd>TroubleToggle loclist<CR>")
