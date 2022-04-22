a0a527b9455f68e7a4b933ffe81635e1a796acc4
Carlos A Becker <caarlos0@gmail.com>
2022-04-21 23:35:36 -0300

    feat: rm git-worktrees
    I'm not really using it anyway... almost always forget about it.
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- nvim/config/lua/user/telescope.lua
+++ nvim/config/lua/user/telescope.lua
@@ -5,7 +5,6 @@ end
 
 telescope.setup({})
 telescope.load_extension("fzf")
-telescope.load_extension("git_worktree")
 
 local r = require("user.remap").nnoremap
 r("<leader>ff", ":Telescope find_files<CR>")
