d0acbac0d039017a47d92b1e5b302c3bd9c59d2f
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-05-23 15:34:03 -0300

    fix: cmp
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/cmp.lua
+++ nvim/config/lua/user/cmp.lua
@@ -27,65 +27,47 @@ cmp.setup({
 		end,
 	},
 	mapping = {
-		-- ["<C-k>"] = cmp.mapping.select_prev_item(),
-		-- ["<C-j>"] = cmp.mapping.select_next_item(),
-		-- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-		-- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		-- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		-- ["<C-e>"] = cmp.mapping({
-		-- 	i = cmp.mapping.abort(),
-		-- 	c = cmp.mapping.close(),
-		-- }),
-		-- Accept currently selected item. If none selected, `select` first item.
-		-- Set `select` to `false` to only confirm explicitly selected items.
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		},
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif check_backspace() then
-				fallback()
+			elseif luasnip.expand_or_jumpable() then
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
 			else
-				cmp.complete()
+				fallback()
 			end
-		end, {
-			"i",
-			"s",
-		}),
+		end, {"i", "s"}),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
 			else
 				fallback()
 			end
-		end, {
-			"i",
-			"s",
-		}),
+		end, {"i", "s"}),
 	},
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = lspkind.cmp_format({
 			mode = "symbol",
-			before = function(entry, item)
-				if entry.source.name == "spell" then
-					item.kind = entry.source.name
-					item.priority = 50
-				end
-				return item
-			end,
 		}),
 	},
 	sources = cmp.config.sources({
-		{ name = "luasnip" },
 		{ name = "nvim_lsp" },
-	}, {
-		{ name = "path" },
+		{ name = "luasnip" },
 		{ name = "buffer" },
-	}, {
-		{ name = "spell" },
-		{ name = "calc" },
-		{ name = "emoji" },
+		{ name = "nvim_lua" },
+		{ name = "path" },
 	}),
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
