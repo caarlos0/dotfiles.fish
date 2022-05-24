5ba228e2338ea3832009bd465c6dc8915257f13e
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-05-23 23:43:53 -0300

    fix: tmux theme
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- tmux/install.fish
+++ tmux/install.fish
@@ -3,5 +3,4 @@ if ! command -qs tmux
 	exit
 end
 
-curl -sL https://raw.githubusercontent.com/catppuccin/tmux/main/catppuccin.conf -o $DOTFILES/tmux/catppuccin.conf
 test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
