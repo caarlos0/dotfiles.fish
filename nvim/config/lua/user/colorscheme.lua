5ca29f19c69d4135e7619ef3e07933bdb302882d
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-05-23 23:43:43 -0300

    fix: improve nvim colors
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/colorscheme.lua
+++ nvim/config/lua/user/colorscheme.lua
@@ -1,8 +1,9 @@
+-- vim.g.gruvbox_contrast_dark = "hard"
+vim.g.gruvbox_sign_column = "dark0"
+
 vim.cmd([[
 set background=dark
 set termguicolors
-let g:gruvbox_italic=1
-let g:gruvbox_contrast_dark='hard'
 colorscheme gruvbox
 ]])
 
--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -43,7 +43,7 @@ return packer.startup(function(use)
 	use("lewis6991/impatient.nvim")
 
 	-- ui
-	use("morhetz/gruvbox")
+	use("gruvbox-community/gruvbox")
 	use("stevearc/dressing.nvim")
 	use("j-hui/fidget.nvim")
 	use("nvim-lualine/lualine.nvim")
