fa425c12d8ba016a292b36192131b19903b5ca10
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-05-23 23:43:23 -0300

    fix: improve fzf theme
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- fzf/install.fish
+++ fzf/install.fish
@@ -1,2 +1,2 @@
 #!/usr/bin/env fish
-set -Ux FZF_DEFAULT_OPTS "--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
+set -Ux FZF_DEFAULT_OPTS "--color=bg+:#282828,bg:#282828,spinner:#d3869b,hl:#83a598 --color=fg:#fbf1c7,header:#83a598,info:#8ec07c,pointer:#d3869b --color=marker:#d3869b,fg+:#ebdbb2,prompt:#8ec07c,hl+:#83a598"
