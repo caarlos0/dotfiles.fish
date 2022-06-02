92e2000021884c820eeb025c453313a945932bb0
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-02 08:59:58 -0300

    fix: exa aliases
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- system/install.fish
+++ system/install.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
 abbr -a less 'less -r'
-if command -qs gpg-connect-agent
-	alias l='exa -lh --icons'
-	alias ll='exa -l --icons'
-	alias lt='exa -l --icons --tree --level=2'
+if command -qs exa
+	abbr -a l 'exa -lh --icons'
+	abbr -a ll 'exa -l --icons'
+	abbr -a lt 'exa -l --icons --tree --level=2'
+	set -Ux EXA_COLORS "da=1;36"
 else
 	abbr -a l 'ls -lAh'
 	abbr -a ll 'ls -l'
