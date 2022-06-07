ebe05203645a76a06afcf5ccb77a58b149130826
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-07 11:54:16 -0300

    fix: do not switch on terms inside nvim
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- tmux/conf.d/tmux-trap.fish
+++ tmux/conf.d/tmux-trap.fish
@@ -1,4 +1,5 @@
 function __trap_exit_tmux
+	test -z "$NVIM_LISTEN_ADDRESS" || exit
 	test (tmux list-windows | wc -l) = 1 || exit
 	test (tmux list-panes | wc -l) = 1 || exit
 	tmux switch-client -t default
