ee0a3ee5044b51d550997386cba6d30cad95f9f7
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-06 21:28:18 -0300

    feat: tmux trap eixt
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

---
+++ tmux/conf.d/tmux-trap.fish
@@ -0,0 +1,10 @@
+function __trap_exit_tmux
+	test (tmux list-windows | wc -l) = 1 || exit
+	test (tmux list-panes | wc -l) = 1 || exit
+	tmux switch-client -t default
+end
+
+if status --is-interactive
+	trap __trap_exit_tmux EXIT
+end
+
