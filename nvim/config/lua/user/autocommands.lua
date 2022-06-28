5c223433ed9f1c2a6a7e052044af5ba661eeeea6
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-27 22:32:50 -0300

    fix(nvim): autocommands in pure lua
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/autocommands.lua
+++ nvim/config/lua/user/autocommands.lua
@@ -1,41 +1,41 @@
-vim.cmd([[
-fun! TrimWhitespace()
-  let l:save = winsaveview()
-  keeppatterns %s/\s\+$//e
-  call winrestview(l:save)
-endfun
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = "*.dockerfile",
+	command = "set ft=dockerfile",
+})
 
-augroup _general_settings
-  autocmd!
-  autocmd BufNewFile,BufRead *.dockerfile set ft=dockerfile
-  autocmd BufWritePre * :call TrimWhitespace()
-  autocmd BufWritePre * call mkdir(expand("<afile>:p:h"), "p")
-  autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
-augroup END
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "gitcommit", "gitrebase" },
+	command = "startinsert | 1",
+})
 
-augroup _git
-  autocmd!
-  autocmd FileType gitcommit setlocal wrap
-  autocmd FileType gitcommit setlocal spell
-augroup END
-
-augroup _markdown
-  autocmd!
-  autocmd FileType markdown setlocal wrap
-  autocmd FileType markdown setlocal spell
-augroup END
-]])
-
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "*.txt", "*.md", "*.tex", "gitcommit", "gitrebase" },
+	command = "setlocal spell",
+})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd(
-	'TextYankPost',
-	{
-		callback = function()
-			vim.highlight.on_yank()
-		end,
-		group = highlight_group,
-		pattern = '*',
-	})
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+		vim.cmd([[ if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif ]])
+	end,
+	pattern = "*",
+})
+
+-- [[ Mkdir, reopen file and trim whitespace on save ]]
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local dir = vim.fn.expand("<afile>:p:h")
+		if vim.fn.isdirectory(dir) == 0 then
+			vim.fn.mkdir(dir, "p")
+			vim.cmd([[e]]) -- reopen file so gopls et al work properly
+		end
+		vim.cmd([[
+			let save = winsaveview()
+			keeppatterns %s/\s\+$//e
+			call winrestview(save)
+		]])
+	end,
+	pattern = "*",
+})
