09bcad11f18539a28022cb159350e0b56339a58e
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-29 00:35:04 -0300

    feat(nvim): treesitter context incremental select
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/treesitter.lua
+++ nvim/config/lua/user/treesitter.lua
@@ -25,6 +25,15 @@ treesitter.setup({
 		enable = true,
 		enable_autocmd = false,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<M-w>", -- maps in normal mode to init the node/scope selection with Alt-w
+			node_incremental = "<M-w>", -- increment to the upper named parent
+			node_decremental = "<M-C-w>", -- decrement to the previous node
+			scope_incremental = "<M-e>", -- increment to the upper scope (as defined in locals.scm)
+		},
+	},
 	textobjects = {
 		enable = true,
 		swap = {
