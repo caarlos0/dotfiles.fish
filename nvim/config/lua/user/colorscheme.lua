01d0cadc77a624de4d74dee5191a83a1449d5f2a
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-07-07 11:13:45 -0300

    fix(nvim): colorscheme
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/colorscheme.lua
+++ nvim/config/lua/user/colorscheme.lua
@@ -1,13 +1,11 @@
 vim.g.gruvbox_contrast_dark = "hard"
--- or dark0_hard:
-vim.g.gruvbox_sign_column = "NONE"
+vim.g.gruvbox_sign_column = "dark0_hard"
 
 vim.cmd([[
 set background=dark
 set termguicolors
 try
-    colorscheme gruvbox
-	hi Normal guibg=NONE ctermbg=NONE
+	colorscheme gruvbox
 catch
 endtry
 ]])
