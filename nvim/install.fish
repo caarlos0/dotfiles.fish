90db792485530a0c7813aaed8bf6a3d1615060d4
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-07-13 11:41:43 -0300

    fix(nvim): improve install
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/install.fish
+++ nvim/install.fish
@@ -1,13 +1,7 @@
 #!/usr/bin/env fish
-ln -sf (which nvim) (which vim)
-	or sudo ln -sf (which nvim) (which vim)
-
+abbr -a e 'nvim'
 abbr -a v 'nvim'
 abbr -a vim 'nvim'
 
-nvim --headless "+PackerSync" "+quit!"
-
-# null-ls formatting for lua
-if command -qs cargo
-	cargo install stylua
-end
+# update plugins
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
