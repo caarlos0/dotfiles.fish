18936b16ed4b76c8bb0c2da236ab2e1caef3fa32
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-07-17 19:34:33 -0300

    feat(nvim): enable more gruvbox features
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/colorscheme.lua
+++ nvim/config/lua/user/colorscheme.lua
@@ -1,5 +1,8 @@
 vim.g.gruvbox_contrast_dark = "hard"
 vim.g.gruvbox_sign_column = "dark0_hard"
+vim.g.gruvbox_italic = 1
+vim.g.gruvbox_underline = 1
+vim.g.gruvbox_undercurl = 1
 
 vim.cmd([[
 set background=dark
