7cba3f47de5a92b37689115be8a7f1ac3be60f3c
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-08-12 20:32:17 -0300

    feat(kitty): gruvbox-material

--- kitty/install.fish
+++ kitty/install.fish
@@ -1,2 +1,2 @@
 #!/usr/bin/env fish
-curl -sL https://raw.githubusercontent.com/wdomitrz/kitty-gruvbox-theme/master/gruvbox_dark.conf -o $DOTFILES/kitty/gruvbox.conf
+curl -sL https://raw.githubusercontent.com/rsaihe/gruvbox-material-kitty/main/colors/gruvbox-material-dark-hard.conf -o $DOTFILES/kitty/gruvbox-material.conf
