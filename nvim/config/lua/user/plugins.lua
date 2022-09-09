65415c7125ea269fb5a16280c6a1e8263c280eb3
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-09-08 22:34:11 -0300

    fix(nvim): fixes session restoring with high cmdline height

--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -30,10 +30,7 @@ packer.startup(function(use)
 	use({
 		"rmagatti/auto-session",
 		config = function()
-			require("auto-session").setup({
-				auto_save_enabled = true,
-				auto_restore_enabled = true,
-			})
+			require("user.session")
 		end,
 	})
 
---
+++ nvim/config/lua/user/session.lua
@@ -0,0 +1,18 @@
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+
+require("auto-session").setup({
+	auto_save_enabled = true,
+	auto_restore_enabled = true,
+	pre_save_cmds = {
+		-- prevents cmdline spanning almost the entire window height on restore.
+		-- this will close all float windows before saving.
+		function()
+			for _, win in ipairs(vim.api.nvim_list_wins()) do
+				local config = vim.api.nvim_win_get_config(win)
+				if config.relative ~= "" then
+					vim.api.nvim_win_close(win, false)
+				end
+			end
+		end,
+	},
+})
