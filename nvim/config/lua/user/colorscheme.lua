422d4915ba87274d2ec2b01be6d4305eae9f7e1b
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-09-11 15:57:07 -0300

    fix(nvim): better comments colors

--- nvim/config/lua/user/colorscheme.lua
+++ nvim/config/lua/user/colorscheme.lua
@@ -1,6 +1,12 @@
 vim.g.catppuccin_flavour = "mocha"
 
-require("catppuccin").setup()
+local colors = require("catppuccin.palettes").get_palette()
+require("catppuccin").setup({
+	custom_highlights = {
+		Comment = { fg = colors.overlay1 },
+		LineNr = { fg = colors.overlay1 },
+	},
+})
 
 vim.cmd([[
 set background=dark
