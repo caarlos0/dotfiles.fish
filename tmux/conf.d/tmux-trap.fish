de85ddd7e0e6512fabf5523dd34d5a1602bd2292
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-09-11 23:48:41 -0300

    fix(nvim): trap tmux on toggleterm

--- tmux/conf.d/tmux-trap.fish
+++ tmux/conf.d/tmux-trap.fish
@@ -1,5 +1,6 @@
 function __trap_exit_tmux
 	test -z "$NVIM_LISTEN_ADDRESS" || exit
+	test -z "$NVIM" || exit
 	test (tmux list-windows | count) = 1 || exit
 	test (tmux list-panes | count) = 1 || exit
 	tmux switch-client -t default
