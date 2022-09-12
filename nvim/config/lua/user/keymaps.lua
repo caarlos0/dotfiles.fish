2f62f64b94b5cd814704df168f96e142715225c2
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-09-11 23:28:55 -0300

    feat(nvim): enable back macros and []space

--- nvim/config/lua/user/keymaps.lua
+++ nvim/config/lua/user/keymaps.lua
@@ -35,17 +35,20 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<leader>w", ":write<CR>", opts)
 keymap("n", "<leader>q", ":Bdelete<CR>", opts)
 
+-- paste over without replacing default register
+keymap("n", "<leader>p", '"_dP', opts)
+
 -- keep more or less in the same place when going next
 keymap("n", "n", "nzzzv", opts)
 keymap("n", "N", "Nzzzv", opts)
 
 -- keymap("n", "Q", "q", opts)
 -- disable macros, I never use them
-keymap("n", "q", "<Nop>", opts)
+-- keymap("n", "q", "<Nop>", opts) - sfs
 
 -- Insert empty blank line above/bellow
--- keymap("n", "[<Space>", "m`o<Esc>``", opts)
--- keymap("n", "]<Space>", "m`O<Esc>``", opts)
+keymap("n", "[<Space>", "m`o<Esc>``", opts)
+keymap("n", "]<Space>", "m`O<Esc>``", opts)
 
 -- Insert --
 -- in insert mode, adds new undo points after , . ! and ?.
