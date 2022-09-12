2773810289754a103c31e1e9a8f9a9a9b712e5e3
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-09-11 23:51:40 -0300

    feat(fzf): catppuccin theme

--- system/install.fish
+++ system/install.fish
@@ -25,3 +25,10 @@ end
 if command -qs zoxide
 	zoxide init fish >$__fish_config_dir/conf.d/zoxide.fish
 end
+
+if command -qs fzf
+	set -Ux FZF_DEFAULT_OPTS "\
+		--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+		--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+		--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+end
