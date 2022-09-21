5cd1870ae5e14c6b800ac201bfedd29bba4952ff
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-09-21 16:00:43 -0300

    feat(nvim): make gf work with lua imports

---
+++ nvim/config/after/ftplugin/lua.lua
@@ -0,0 +1,9 @@
+-- Options to add `gf` functionality inside `.lua` files.
+vim.opt_local.include = [[\v<((do|load)file|require)[^''"]*[''"]\zs[^''"]+]]
+vim.opt_local.includeexpr = "substitute(v:fname,'\\.','/','g')"
+
+for _, path in pairs(vim.api.nvim_list_runtime_paths()) do
+	vim.opt_local.path:append(path .. "/lua")
+end
+
+vim.opt_local.suffixesadd:prepend(".lua")
