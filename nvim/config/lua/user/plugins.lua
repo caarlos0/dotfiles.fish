2f88f22866e749d9567cad26408fe023cab2e351
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-10-12 22:11:56 -0300

    fix(nvim): auto packer compile on save plugins.lua
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -306,8 +306,8 @@ if is_bootstrap then
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
--- vim.api.nvim_create_autocmd("BufWritePost", {
--- 	command = "source <afile> | PackerSync",
--- 	group = vim.api.nvim_create_augroup("Packer", { clear = true }),
--- 	pattern = "plugins.lua",
--- })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	command = "source <afile> | PackerCompile",
+	group = vim.api.nvim_create_augroup("Packer", { clear = true }),
+	pattern = "plugins.lua",
+})
