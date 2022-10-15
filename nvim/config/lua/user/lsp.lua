b34456a6b37d86422fcc21c659e34eb516e80f56
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-10-15 16:59:49 -0300

    fix(nvim): remove deprecated usage

--- nvim/config/lua/user/lsp.lua
+++ nvim/config/lua/user/lsp.lua
@@ -1,6 +1,6 @@
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities()) or {}
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
 capabilities.textDocument.completion.completionItem = {
 	documentationFormat = { "markdown", "plaintext" },
