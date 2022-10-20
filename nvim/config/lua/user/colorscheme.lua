6018a8ad8dd069e1fb003032d95d8089f797e686
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-10-20 01:01:43 -0300

    feat(nvim): enable catppuccin integrations

--- nvim/config/lua/user/colorscheme.lua
+++ nvim/config/lua/user/colorscheme.lua
@@ -6,6 +6,31 @@ require("catppuccin").setup({
 		Comment = { fg = colors.overlay1 },
 		LineNr = { fg = colors.overlay1 },
 	},
+	integrations = {
+		cmp = true,
+		dap = {
+			enabled = true,
+			enable_ui = true,
+		},
+		fidget = true,
+		gitsigns = true,
+		indent_blankline = {
+			enabled = true,
+			colored_indent_levels = false,
+		},
+		lsp_trouble = true,
+		markdown = true,
+		mason = true,
+		neogit = true,
+		neotree = true,
+		notify = true,
+		nvimtree = true,
+		symbols_outline = true,
+		telescope = true,
+		treesitter = true,
+		treesitter_context = true,
+		which_key = true,
+	},
 })
 
 vim.cmd([[
