fdb3295d87291949c57e0704d930fa723de9ec44
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-10-20 11:31:11 -0300

    fix(nvim): autosession be quiet

--- nvim/config/lua/user/session.lua
+++ nvim/config/lua/user/session.lua
@@ -1,6 +1,7 @@
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
 
 require("auto-session").setup({
+	log_level = "error",
 	auto_save_enabled = true,
 	auto_restore_enabled = true,
 	pre_save_cmds = {
