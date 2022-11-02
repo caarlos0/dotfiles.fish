6c37d7fdafbcc54759041086d16bb8b7e981fd44
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-11-01 21:24:17 -0300

    fix(fzf): amoled
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- system/install.fish
+++ system/install.fish
@@ -28,7 +28,7 @@ end
 
 if command -qs fzf
 	set -Ux FZF_DEFAULT_OPTS "\
-		--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+		--color=bg:#000000,bg+:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 		--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 		--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 end
