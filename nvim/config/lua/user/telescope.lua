6109cb5beb0bc1e93530a6332267c6b2c6492408
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-11-06 20:39:36 -0300

    feat(nvim): improve telescope config

--- nvim/config/lua/user/telescope.lua
+++ nvim/config/lua/user/telescope.lua
@@ -1,39 +1,23 @@
-local actions = require("telescope.actions")
 local telescope = require("telescope")
 
 telescope.setup({
 	defaults = {
-		mappings = {
-			i = {
-				["<esc>"] = actions.close,
-				["<Tab>"] = actions.move_selection_previous,
-				["<S-Tab>"] = actions.move_selection_next,
-			},
-		},
-		vimgrep_arguments = {
-			"rg",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-			"--hidden",
-			"--glob=!.git",
-		},
 		prompt_prefix = "   ",
-		color_devicons = true,
 	},
 })
 
+local builtin = require("telescope.builtin")
 local r = require("user.remap").nnoremap
-r("<C-p>", ":Telescope find_files find_command=rg,--hidden,--files,--smart-case,--glob=!.git<CR>")
-r("<leader>of", ":Telescope oldfiles<CR>")
-r("<leader>lg", ":Telescope live_grep<CR>")
-r("<leader>fb", ":Telescope buffers<CR>")
-r("<leader>fh", ":Telescope help_tags<CR>")
-r("<leader>ft", ":Telescope treesitter<CR>")
-r("<leader>fc", ":Telescope commands<CR>")
-r("<leader>fr", ":Telescope resume<CR>")
-r("<leader>fq", ":Telescope quickfix<CR>")
-r("<leader>/", ":Telescope current_buffer_fuzzy_find<CR>")
+r("<C-p>", function()
+	builtin.find_files({
+		hidden = true,
+	})
+end)
+r("<leader>of", builtin.oldfiles)
+r("<leader>lg", builtin.live_grep)
+r("<leader>fb", builtin.buffers)
+r("<leader>fh", builtin.help_tags)
+r("<leader>fc", builtin.commands)
+r("<leader>fr", builtin.resume)
+r("<leader>fq", builtin.quickfix)
+r("<leader>/", builtin.current_buffer_fuzzy_find)
