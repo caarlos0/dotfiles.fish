fd62ca9c691b2d10a698e1e51618252353d8b1ef
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-11-06 20:31:12 -0300

    fix(nvim): use vim.keymap

--- nvim/config/lua/user/remap.lua
+++ nvim/config/lua/user/remap.lua
@@ -1,10 +1,9 @@
-local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 local M = {}
 
 M.nnoremap = function(key, cmd)
-	keymap("n", key, cmd, opts)
+	vim.keymap.set("n", key, cmd, opts)
 end
 
 return M
