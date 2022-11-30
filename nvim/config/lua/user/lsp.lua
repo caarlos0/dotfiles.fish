76746b3bc45c4ec00a916d4f33cc31929d584bed
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-11-30 10:12:51 -0300

    fix(nvim): improve lua lsp configs

--- nvim/config/lua/user/lsp.lua
+++ nvim/config/lua/user/lsp.lua
@@ -173,14 +173,27 @@ lspconfig.dockerls.setup({
 	on_attach = on_attach,
 })
 
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 lspconfig.sumneko_lua.setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = {
 		Lua = {
-			diagnostics = {
-				globals = { "vim", "require", "pcall", "pairs" },
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = runtime_path,
 			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = { enable = false },
 			hint = {
 				enable = true,
 			},
