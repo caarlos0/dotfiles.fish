55a0583c8ce22cf80bf89f8dcaaad23de1aef00c
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-12-12 09:41:48 -0300

    fix(nvim): improve some keymaps

--- nvim/config/lua/user/keymaps.lua
+++ nvim/config/lua/user/keymaps.lua
@@ -51,13 +51,13 @@ keymap("n", "N", "Nzzzv", opts)
 keymap("n", "<C-u>", "<C-u>zz", opts)
 keymap("n", "<C-d>", "<C-d>zz", opts)
 
--- disable macros, I never use them
--- keymap("n", "Q", "q", opts)
--- keymap("n", "q", "<Nop>", opts) - sfs
+-- move record macro to Q instead of q
+keymap("n", "Q", "q", opts)
+keymap("n", "q", "<Nop>", opts)
 
 -- Insert empty blank line above/bellow
-keymap("n", "[<Space>", "m`o<Esc>``", opts)
-keymap("n", "]<Space>", "m`O<Esc>``", opts)
+keymap("n", "]<Space>", "m`o<Esc>``", opts)
+keymap("n", "[<Space>", "m`O<Esc>``", opts)
 
 -- Insert --
 -- in insert mode, adds new undo points after , . ! and ?.
