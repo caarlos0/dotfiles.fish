aaf57537d81ff7c024a9dfe83c960ddbd30ad487
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-12-22 14:10:44 -0300

    fix(fish): dont need the extra completions file anymore
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- docker/install.fish
+++ docker/install.fish
@@ -1,5 +1,3 @@
 #!/usr/bin/env fish
-curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish -o $__fish_config_dir/docker.fish
-
 abbr -a d docker
 abbr -a dc docker-compose
