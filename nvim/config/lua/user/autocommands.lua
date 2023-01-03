dea17469a77f2347fec4694b43c34122b1d089ee
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-03 13:40:05 -0300

    feat(nvim): autocmd to jump to last place a buffer was opened

--- nvim/config/lua/user/autocommands.lua
+++ nvim/config/lua/user/autocommands.lua
@@ -49,3 +49,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
   group = vim.api.nvim_create_augroup("Highlight", { clear = true }),
 })
+
+-- [[ Jump to the " mark when reading a buffer ]]
+-- It will search mark " which contains the cursor position when last
+-- exiting the buffer and set the cursor position to that location.
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+  group = vim.api.nvim_create_augroup("OpenInLastPosition", { clear = true }),
+})
