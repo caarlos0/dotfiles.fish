c856299c9d942f8e3fbba736d4bea44ea829aac5
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-06 00:44:34 -0300

    fix(nvim): function with fewer params

--- nvim/config/lua/user/cmp.lua
+++ nvim/config/lua/user/cmp.lua
@@ -33,7 +33,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-Space>"] = cmp.mapping.complete({}),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
--- nvim/config/lua/user/lsp.lua
+++ nvim/config/lua/user/lsp.lua
@@ -194,7 +194,7 @@ lspconfig.clangd.setup({
 })
 
 -- Make runtime files discoverable to the server
-local runtime_path = vim.split(package.path, ";")
+local runtime_path = vim.split(package.path, ";", {})
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 lspconfig.sumneko_lua.setup({
