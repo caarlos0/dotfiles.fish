eedd396d850ad1bae76c1caa11f214ba19f1ad12
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-11 21:26:26 -0300

    fix(nvim): gitsigns actions
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- nvim/config/lua/user/lsp.lua
+++ nvim/config/lua/user/lsp.lua
@@ -259,6 +259,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
+    null_ls.builtins.code_actions.gitsigns,
   },
   capabilities = capabilities,
   on_attach = on_attach,
