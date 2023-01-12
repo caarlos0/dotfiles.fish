7340e18c502a602d591973745aed5ad4004c0e19
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-11 21:55:31 -0300

    feat(nvim): format fish and shfmt with null-ls

--- nvim/config/lua/user/lsp.lua
+++ nvim/config/lua/user/lsp.lua
@@ -259,7 +259,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.code_actions.gitsigns,
+    -- null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.fish_indent,
+    null_ls.builtins.formatting.shfmt,
   },
   capabilities = capabilities,
   on_attach = on_attach,
