454bafdf119918ff648e15a1489faabcc30715e8
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-23 11:45:07 -0300

    feat(nvim): improve notify settings

--- nvim/config/lua/user/notify.lua
+++ nvim/config/lua/user/notify.lua
@@ -2,8 +2,23 @@ local notify = require("notify")
 notify.setup({
   render = "minimal",
   stages = "fade",
+  timeout = 3000,
+  max_height = function()
+    return math.floor(vim.o.lines * 0.75)
+  end,
+  max_width = function()
+    return math.floor(vim.o.columns * 0.75)
+  end,
   on_open = function(win)
     vim.api.nvim_win_set_config(win, { focusable = false })
   end,
 })
 vim.notify = notify
+
+vim.keymap.set("n", "<leader>un", function()
+  notify.dismiss({ silent = true, pending = true })
+end, {
+  noremap = true,
+  silent = true,
+  desc = "Delete all Notifications",
+})
