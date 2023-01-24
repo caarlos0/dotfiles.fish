f96feadf9008b16b9fce16760b076ea9a44c5578
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-23 23:31:29 -0300

    feat(nvim): improve treesitter configs

--- nvim/config/lua/plugins/treesitter.lua
+++ nvim/config/lua/plugins/treesitter.lua
@@ -13,7 +13,7 @@ return {
           enable = true,
         },
         indent = {
-          enable = false,
+          enable = true,
         },
         ensure_installed = {
           "bash",
@@ -63,10 +63,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "<M-w>", -- maps in normal mode to init the node/scope selection with Alt-w
-            node_incremental = "<M-w>", -- increment to the upper named parent
-            node_decremental = "<M-C-w>", -- decrement to the previous node
-            scope_incremental = "<M-e>", -- increment to the upper scope (as defined in locals.scm)
+            init_selection = "<C-space>", -- maps in normal mode to init the node/scope selection with Alt-w
+            node_incremental = "<C-space>", -- increment to the upper named parent
+            node_decremental = "<bs>", -- decrement to the previous node
+            scope_incremental = "<noop>", -- increment to the upper scope (as defined in locals.scm)
           },
         },
         textobjects = {
