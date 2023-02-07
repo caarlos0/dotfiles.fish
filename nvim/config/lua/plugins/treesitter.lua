a89cdb9e5aef8c8d779afeeb394d2ce183c774b5
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-02-07 11:28:57 -0300

    chore(nvim): fix comment

--- nvim/config/lua/plugins/treesitter.lua
+++ nvim/config/lua/plugins/treesitter.lua
@@ -31,7 +31,7 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "<C-space>", -- maps in normal mode to init the node/scope selection with Alt-w
+            init_selection = "<C-space>", -- maps in normal mode to init the node/scope selection with ctrl+space
             node_incremental = "<C-space>", -- increment to the upper named parent
             node_decremental = "<bs>", -- decrement to the previous node
             scope_incremental = "<noop>", -- increment to the upper scope (as defined in locals.scm)
